ac9f617c1660a2488a84c650f1cbfcdac273b02b
biudiu <805817422@qq.com>
2020-11-19 09:27:28 +0800

    连接，添加，删除，查看

--- src/com/company/JDBCDemo4.java
+++ src/com/company/JDBCDemo4.java
@@ -2,12 +2,13 @@ package com.company;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
  * JDBC连接mysql实例
  */
-public class JDBCDemo1 {
+public class JDBCDemo4 {
     public static void main(String[] args) throws Exception {
         //        1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         //        2、 注册驱动
@@ -15,16 +16,18 @@ public class JDBCDemo1 {
         //        3、获取数据库连接对象
         Connection conn=DriverManager.getConnection("jdbc:mysql://"+"localhost:3306/jiangkeda","root","root");
         //        4、定义sql
-        String sql="update account set balance=500 where id=1";
+        String sql="select * from account";
         //        5、获取数据库连接对象statement
         Statement stmt=conn.createStatement();
         //        6、执行sql
-        int i=stmt.executeUpdate(sql);
+        ResultSet rs=stmt.executeQuery(sql);
         //        7、处理结果
-        if(i==1){
-            System.out.println("更新成功");
-        }else{
-            System.out.println("失败");
+
+        while(rs.next()) {
+            int id = rs.getInt("ID");
+            String name = rs.getString("name");
+            double balance = rs.getDouble("balance");
+            System.out.println(id + "------" + name + "------" + balance);
         }
         //        8、释放资源
         stmt.close();
